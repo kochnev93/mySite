1448f8d7c47f305542ff2fde3a8a046be09fc25c
kochnev93 <kochnev93@mail.ru>
2023-04-13 18:22:47 +0300

    bg

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -8,36 +8,38 @@ const Background: React.FC = () => {
   const squareRef = useRef(null)
 
   useEffect(() => {
-    const colors = ['#7F152E', '#D61800', '#EDAE01', '#E94F08'];
+  //   const colors = ['#7F152E', '#D61800', '#EDAE01', '#E94F08'];
 
-    let innerWidth =  Math.floor(window.innerWidth / widthBox);
-    let innerHeight =  Math.floor(window.innerHeight / widthBox);
-    let count = innerWidth * innerHeight;
+  //   let innerWidth =  Math.floor(window.innerWidth / widthBox);
+  //   let innerHeight =  Math.floor(window.innerHeight / widthBox);
+  //   let count = innerWidth * innerHeight;
+
+  //   squareRef.current.innerHTNL = '';
 
 
-  for(let i = 0; i < count; i++){
-    const box = document.createElement('div');
-    box.classList.add('square');
-    box.addEventListener('mouseover', () => setColor(square))
-    box.addEventListener('mouseleave', () => removeColor(square))
-    squareRef.current.append(box);
-  }
+  // for(let i = 0; i < count; i++){
+  //   const box = document.createElement('div');
+  //   box.classList.add('box');
+  //   box.addEventListener('mouseover', () => setColor(square))
+  //   box.addEventListener('mouseleave', () => removeColor(square))
+  //   squareRef.current.append(box);
+  // }
 
-  function setColor(element){
-    const color = getRandomColor();
-    element.style.background = color;
-    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
-  }
+  // function setColor(element: any){
+  //   const color = getRandomColor();
+  //   element.style.background = color;
+  //   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+  // }
 
-  function removeColor(element){
-      element.style.background = '#1d1d1d';
-      element.style.boxShadow = `0 0 2px #000`;
-  }
+  // function removeColor(element: any){
+  //     element.style.background = '#1d1d1d';
+  //     element.style.boxShadow = `0 0 2px #000`; 
+  // }
 
-  function getRandomColor(){
-      const index = Math.floor(Math.random() * colors.length);
-      return colors[index];
-  }
+  // function getRandomColor(){
+  //     const index = Math.floor(Math.random() * colors.length);
+  //     return colors[index];
+  // }
 
 
 
@@ -56,15 +58,7 @@ const Background: React.FC = () => {
     <div className={styles.light}></div>
     <div className={styles.light}></div>
     <div className={styles.light}></div>
-    <div className={styles.square} ref={squareRef}>
-{Array(countBox).fill(true).map((item, index) => {
-  return(
-    <div key={index} className={styles.box} />
-  )
-})}
-
-
-    </div>
+    <div className={styles.square} ref={squareRef} />
   </div>
 )};
 
