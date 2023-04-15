2654ad5ac7f803e5b2724a3968d8cd38896300ba
kochnev93 <kochnev93@mail.ru>
2023-04-15 18:04:27 +0300

    deploy

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,16 @@
 import { FC } from 'react';
-import { ReactNode } from 'react';
+// import { AiFillHeart } from 'react-icons/Ai';
 
-import {AiFillHeart} from 'react-icons/Ai'
-
-import styles from './Footer.module.scss'
+import styles from './Footer.module.scss';
 
 const Footer: FC = () => {
-  return <footer className={styles.footer}>
-    	&#169; {new Date().getFullYear()}, Разработано&nbsp;<a href="https://github.com/kochnev93">мной</a>&nbsp;с&nbsp;<AiFillHeart/>
-  </footer>;
+  return (
+    <footer className={styles.footer}>
+      &#169; {new Date().getFullYear()}, Разработано&nbsp;
+      <a href="https://github.com/kochnev93">мной</a>&nbsp;с&nbsp;
+      {/* <AiFillHeart /> */}
+    </footer>
+  );
 };
 
 export default Footer;
