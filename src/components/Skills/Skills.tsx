67624cc1daeacc2b654fbcbbea608db117373949
Kochnev Anton <alexsander-bk@bk.ru>
2024-01-17 22:37:04 +0300

    update cv, add new skills

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -5,14 +5,17 @@ import { useInView } from 'framer-motion';
 //Icons
 import { FaReact } from 'react-icons/fa';
 import { SiNextdotjs } from 'react-icons/si';
-import {TbBrandJavascript} from 'react-icons/tb'
-import {TbBrandTypescript} from 'react-icons/tb'
-import {SiMysql} from 'react-icons/si'
-import {SiExpress} from 'react-icons/si'
-import {AiOutlineHtml5} from 'react-icons/ai'
-import {BsFiletypeScss} from 'react-icons/bs'
-import {BsWordpress} from 'react-icons/bs'
+import { TbBrandJavascript } from 'react-icons/tb';
+import { TbBrandTypescript } from 'react-icons/tb';
+import { SiMysql } from 'react-icons/si';
+import { SiExpress } from 'react-icons/si';
+import { AiOutlineHtml5 } from 'react-icons/ai';
+import { BsFiletypeScss } from 'react-icons/bs';
+import { BsWordpress } from 'react-icons/bs';
 
+import { BiLogoMongodb } from 'react-icons/bi';
+import { SiMinio } from 'react-icons/si';
+import { SiGatsby } from 'react-icons/si';
 
 //Styles
 import styles from './Skills.module.scss';
@@ -25,13 +28,17 @@ const Skills: FC = () => {
   const isInView = useInView(ref, { once: true });
 
   return (
-    <ul className={cx(styles.skills, {[styles.inview]: isInView})} ref={ref}>
+    <ul className={cx(styles.skills, { [styles.inview]: isInView })} ref={ref}>
       <Skill title="React JS" subtitle="React Hooks, Redux-toolkit">
         <FaReact />
       </Skill>
 
-      <Skill title="Next JS">
+      <Skill title="Next.js">
         <SiNextdotjs />
+      </Skill>
+
+      <Skill title="Gatsby.js">
+        <SiGatsby />
       </Skill>
 
       <Skill title="JavaScript" subtitle="ES5, ES6, Promise">
@@ -54,8 +61,16 @@ const Skills: FC = () => {
         <SiMysql />
       </Skill>
 
+      <Skill title="Mongo DB">
+        <BiLogoMongodb />
+      </Skill>
+
       <Skill title="Express JS">
         <SiExpress />
+      </Skill>
+
+      <Skill title="Minio">
+        <SiMinio />
       </Skill>
 
       <Skill title="Wordpress">
